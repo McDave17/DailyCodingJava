599b33a32915f48e19cce5feca1713b9dad1daf8
McDave17 <mcdermottdavid396@gmail.com>
2020-12-01 22:33:01 +0100

    task: 565 bugfix: invalid output with only one value in int array

--- src/de/mcdermott/dailycoding/challange565/DailyCoding565.java
+++ src/de/mcdermott/dailycoding/challange565/DailyCoding565.java
@@ -12,9 +12,10 @@ public class DailyCoding565 {
     private static final int[][] input;
 
     static {
-        input = new int[2][];
-        input[0] = new int[]{2, 0, 1, 0};
-        input[1] = new int[]{1, 1, 0, 1};
+        input = new int[3][];
+        input[0] = new int[]{0};
+        input[1] = new int[]{2, 0, 1, 0};
+        input[2] = new int[]{1, 1, 0, 1};
     }
 
     public static void main(String[] args) {
@@ -24,13 +25,13 @@ public class DailyCoding565 {
 
     private static boolean execute(int[] input) {
         for (int i = 0; i < input.length; ) {
+            if (i == input.length - 1)
+                return true;
+
             if (input[i] == 0)
                 return false;
 
             i += input[i];
-
-            if (i == input.length - 1)
-                return true;
         }
 
         return false;
